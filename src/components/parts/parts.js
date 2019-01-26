69ac576282389fbb8afc9d4022d97a1be4f5414f
emeryparks <emeryparks09@hotmail.com>
2019-01-25 19:50:04 -0800

    added parts table columns

--- src/components/parts/parts.js
+++ src/components/parts/parts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // import { render } from 'react-dom';
+import matchSorter from 'match-sorter';
 import { makeData, Tips } from './utils';
 import _ from 'lodash';
 
@@ -14,6 +15,83 @@ const columns = [
       {
         Header: 'First Name',
         accessor: 'firstName',
+      },
+      {
+        Header: 'ID',
+        accessor: 'part_id',
+        filterMethod: (filter, rows) =>
+          matchSorter(rows, filter.value, { keys: ['part_id'] }),
+        filterAll: true,
+      },
+      {
+        Header: 'Description',
+        accessor: 'part_desc',
+        filterMethod: (filter, rows) =>
+          matchSorter(rows, filter.value, { keys: ['part_desc'] }),
+        filterAll: true,
+      },
+      {
+        Header: 'Subpart?',
+        accessor: 'part_sub',
+        filterMethod: (filter, rows) =>
+          matchSorter(rows, filter.value, { keys: ['part_sub'] }),
+        filterAll: true,
+      },
+      {
+        Header: 'Source',
+        accessor: 'part_src',
+        filterMethod: (filter, rows) =>
+          matchSorter(rows, filter.value, { keys: ['part_src'] }),
+        filterAll: true,
+      },
+      {
+        Header: 'Manufacture Part Number',
+        accessor: 'part_mfgnum',
+        filterMethod: (filter, rows) =>
+          matchSorter(rows, filter.value, { keys: ['part_mfgnum'] }),
+        filterAll: true,
+      },
+      {
+        Header: 'Price',
+        accessor: 'part_price',
+        filterMethod: (filter, rows) =>
+          matchSorter(rows, filter.value, { keys: ['part_price'] }),
+        filterAll: true,
+      },
+      {
+        Header: 'Category ID',
+        accessor: 'part_category',
+        filterMethod: (filter, rows) =>
+          matchSorter(rows, filter.value, { keys: ['part_category'] }),
+        filterAll: true,
+      },
+      {
+        Header: 'Location ID',
+        accessor: 'part_location',
+        filterMethod: (filter, rows) =>
+          matchSorter(rows, filter.value, { keys: ['part_location'] }),
+        filterAll: true,
+      },
+      {
+        Header: 'Part Count',
+        accessor: 'part_count',
+        filterMethod: (filter, rows) =>
+          matchSorter(rows, filter.value, { keys: ['part_count'] }),
+        filterAll: true,
+      },
+      {
+        Header: 'Long lead?',
+        accessor: 'part_longlead',
+        filterMethod: (filter, rows) =>
+          matchSorter(rows, filter.value, { keys: ['part_longlead'] }),
+        filterAll: true,
+      },
+      {
+        Header: 'Notes',
+        accessor: 'part_notes',
+        filterMethod: (filter, rows) =>
+          matchSorter(rows, filter.value, { keys: ['part_notes'] }),
+        filterAll: true,
       },
       {
         Header: 'Last Name',
