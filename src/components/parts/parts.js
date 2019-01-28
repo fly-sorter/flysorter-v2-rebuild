d57e9a5a4c083e9040f3a0929dd731c6efb6b6b9
emeryparks <emeryparks09@hotmail.com>
2019-01-28 13:44:01 -0800

    DRYied up parts.js filterMethod repetitiveness

--- src/components/parts/parts.js
+++ src/components/parts/parts.js
@@ -15,79 +15,46 @@ const columns = [
       {
         Header: 'ID',
         accessor: 'part_id',
-        filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ['part_id'] }),
-        filterAll: true,
       },
       {
         Header: 'Description',
         accessor: 'part_desc',
-        filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ['part_desc'] }),
-        filterAll: true,
       },
       {
         Header: 'Subpart?',
         accessor: 'part_sub',
-        filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ['part_sub'] }),
-        filterAll: true,
       },
       {
         Header: 'Source',
         accessor: 'part_src',
-        filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ['part_src'] }),
-        filterAll: true,
       },
       {
         Header: 'Mfr. Part #',
         accessor: 'part_mfgnum',
-        filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ['part_mfgnum'] }),
-        filterAll: true,
       },
       {
         Header: 'Price',
         accessor: 'part_price',
-        filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ['part_price'] }),
-        filterAll: true,
       },
       {
         Header: 'Category ID',
         accessor: 'part_category',
-        filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ['part_category'] }),
-        filterAll: true,
       },
       {
         Header: 'Location ID',
         accessor: 'part_location',
-        filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ['part_location'] }),
-        filterAll: true,
       },
       {
         Header: 'Part Count',
         accessor: 'part_count',
-        filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ['part_count'] }),
-        filterAll: true,
       },
       {
         Header: 'Long lead?',
         accessor: 'part_longlead',
-        filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ['part_longlead'] }),
-        filterAll: true,
       },
       {
         Header: 'Notes',
         accessor: 'part_notes',
-        filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ['part_notes'] }),
-        filterAll: true,
       },
     ],
   },
@@ -105,14 +72,17 @@ export default class Parts extends React.Component {
     return (
       <div>
         <ReactTable
-          filterable
-          data={data}
+          className="-striped -highlight parts-table"
           columns={columns}
+          data={data}
           defaultPageSize={25}
+          filterMethod={(filter, rows) => {
+            return matchSorter(rows, filter.value);
+          }}
+          filterable
           style={{
             height: '670px',
           }}
-          className="-striped -highlight parts-table"
           SubComponent={row => {
             return (
               <div style={{ padding: '20px' }}>
@@ -124,8 +94,8 @@ export default class Parts extends React.Component {
                 <br />
                 <ReactTable
                   className='parts-table'
-                  data={data}
                   columns={columns}
+                  data={data}
                   filterable
                   defaultPageSize={5}
                   showPagination={false}
